146c1742adc48ae9ba9e0968722c9f269b71db7f
Chris Shieh <dev@chrisgrace.com>
2023-01-09 23:47:51 -0800

    regex book

--- regex.rb
+++ regex.rb
@@ -1,3 +1,7 @@
-str = 'Doc in a big red box.'
-arr = str.scan(/\s...\s/)
-p arr
+def format_date(str)
+  str.sub(/\A(\d\d\d\d)([\-\/])(\d\d)\2(\d\d)\z/, '\4.\3.\1')
+end
+
+puts format_date('2016-06-17') == '17.06.2016'
+puts format_date('2017/05/03') == '03.05.2017'
+puts format_date('2015/01-31') == '2015/01-31'
